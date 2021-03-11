cbc91e52953b67cffb884784b3e250c7dc0007a9
lamak <lamak.qaizar@careem.com>
2021-03-11 19:17:10 +0500

    test clean up: removed recurring cli set up

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 from main import process_git_command
 
+
 class TestCLI:
     def __init__(self):
         self.commands = []
@@ -16,34 +17,35 @@ class TestCLI:
         test_case.assertListEqual(self.commands, commands)
         test_case.assertListEqual(self.messages, messages)
 
+
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.cli = TestCLI()
+
     def test_commit(self):
-        cli = TestCLI()
-        process_git_command(["/path/to/script", "no-push", "hello"], cli)
-        cli.assertInteraction(self,
+        process_git_command(["/path/to/script", "no-push", "hello"], self.cli)
+        self.cli.assertInteraction(self,
             commands = ['git add .',
              'git commit -m "hello"'])
 
     def test_push(self):
-        cli = TestCLI()
-        process_git_command(["/path/to/script", "push", "hello"], cli)
-        cli.assertInteraction(self,
+        process_git_command(["/path/to/script", "push", "hello"], self.cli)
+        self.cli.assertInteraction(self,
             commands = ['git add .',
              'git commit -m "hello"',
              'git push'])
 
     def test_push_with_coauthors(self):
-        cli = TestCLI()
-        process_git_command(["/path/to/script", "push", "hello", "co:fahad,haris"], cli)
-        cli.assertInteraction(self,
+        process_git_command(["/path/to/script", "push", "hello", "co:fahad,haris"], self.cli)
+        self.cli.assertInteraction(self,
             commands = ['git add .',
              'git commit -m "hello\n\nCo-authored-by: Fahad Khan <fahad.khan@careem.com>\nCo-authored-by: Haris Khan <haris.khan@careem.com>"',
              'git push'])
 
     def test_invalid_initials(self):
-        cli = TestCLI()
-        process_git_command(["/path/to/script", "push", "hello", "co:blah"], cli)
-        cli.assertInteraction(self, messages = ['Invalid co-author initials. Registered initials: fahad, haris, faiq, arham, talha, saad.i, shariq, saad.a'])
+        process_git_command(["/path/to/script", "push", "hello", "co:blah"], self.cli)
+        self.cli.assertInteraction(self, messages = ['Invalid co-author initials. Registered initials: fahad, haris, faiq, arham, talha, saad.i, shariq, saad.a'])
+
 
 if __name__ == '__main__':
     unittest.main()
