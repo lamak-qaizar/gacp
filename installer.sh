0c8d5dce5b810240fdf5b46e0a3057630b725eac
lamak <lamak.qaizar@gmail.com>
2022-07-24 13:35:59 +0500

    Added logs

--- installer.sh
+++ installer.sh
@@ -3,25 +3,30 @@
 gacp_dir="$HOME/gacp"
 
 function clone_gacp() {
+  echo "Cloning GACP unless it is already cloned under $gacp_dir."
   if [[ ! -e $gacp_dir ]]; then
       git -C $HOME clone https://github.com/lamakq/gacp.git
   fi
 }
 
 function update_gacp() {
+  echo "Pulling GACP unless it is already cloned under $gacp_dir."
   git -C $gacp_dir pull
 }
 
 function create_initials_json() {
+  echo "Creating initials.json unless it is already created."
   if [[ ! -e $gacp_dir/initials.json ]]; then
       echo "{}" > $gacp_dir/initials.json
   fi
 }
 
 function register_gacp_in_path() {
+  echo "Registering gacp in \$PATH unless it already there. \$PATH=$PATH"
   if [[ $PATH != *"$gacp_dir/bin"* ]]; then
     echo "export PATH=$PATH:$gacp_dir/bin" >> ~/.bash_profile
     source ~/.bash_profile
+    echo "~/.bash_profile has been updated. $gacp_dir/bin has been registered in PATH. \$PATH=$PATH"
   fi
 }
 
